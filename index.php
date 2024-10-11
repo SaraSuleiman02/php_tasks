8e553aa7657b64050d652fb2bf47a2781bfe20ea
SaraSuleiman02 <sarasuleiman1234@gmail.com>
2024-10-11 19:38:37 +0200

    Add index.php and style.css

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <h1>Php Tasks Solution</h1>
+    <div class="container">
+        <h3>Folder 1</h3>
+
+        <h4 onclick="toggleLinks('arrayTasks')">Array Tasks</h4>
+        <div class="links" id="arrayTasks">
+            <div class="link-box">
+                <a href="./Folder1/Array/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task5.php">Task 5</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task6.php">Task 6</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task7.php">Task 7</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Array/task8.php">Task 8</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('functionTasks')">Function Tasks</h4>
+        <div class="links" id="functionTasks">
+            <div class="link-box">
+                <a href="./Folder1/Function/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Function/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Function/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Function/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Function/task5.php">Task 5</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Function/task6.php">Task 6</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('LogicalTasks')">Logical Statements and Operators Tasks</h4>
+        <div class="links" id="LogicalTasks">
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task5.php">Task 5</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task6.php">Task 6</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task7.php">Task 7</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task8.php">Task 8</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/Logical Statements/task9.php">Task 9</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('stringTasks')">String Tasks</h4>
+        <div class="links" id="stringTasks">
+            <div class="link-box">
+                <a href="./Folder1/String/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task5.php">Task 5</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task6.php">Task 6</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task7.php">Task 7</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder1/String/task8.php">Task 8</a>
+            </div>
+        </div>
+
+
+        <h3>Folder 2</h3>
+
+        <h4 onclick="toggleLinks('stringTasks2')">String Tasks</h4>
+        <div class="links" id="stringTasks2">
+            <div class="link-box">
+                <a href="./Folder2/String/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task5.php">Task 5</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task6.php">Task 6</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task7.php">Task 7</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task8.php">Task 8</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task9.php">Task 9</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task10.php">Task 10</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/String/task11.php">Task 11</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('loopTasks2')">Loop Tasks</h4>
+        <div class="links" id="loopTasks2">
+            <div class="link-box">
+                <a href="./Folder2/Loop/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Loop/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Loop/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Loop/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Loop/task5.php">Task 5</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('logicalTasks2')">Logical Statements and Operators Tasks</h4>
+        <div class="links" id="logicalTasks2">
+            <div class="link-box">
+                <a href="./Folder2/Logical operators/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Logical operators/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Logical operators/task3.php">Task 3</a>
+            </div>
+        </div>
+
+        <h4 onclick="toggleLinks('arrayTasks2')">Array Tasks</h4>
+        <div class="links" id="arrayTasks2">
+            <div class="link-box">
+                <a href="./Folder2/Array/task1.php">Task 1</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Array/task2.php">Task 2</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Array/task3.php">Task 3</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Array/task4.php">Task 4</a>
+            </div>
+            <div class="link-box">
+                <a href="./Folder2/Array/task5.php">Task 5</a>
+            </div>
+        </div>
+
+        <h3>Class Tasks</h3>
+        <h4 onclick="toggleLinks('classTask1')">Class Task1</h4>
+        <div class="links" id="classTask1">
+            <div class="link-box">
+                <a href="./class_tasks/task1.php">Check it!</a>
+            </div>
+        </div>
+
+        <h3>Advanced Task</h3>
+        <h4 onclick="toggleLinks('advancedTask')">Advanced Task 1</h4>
+        <div class="links" id="advancedTask">
+            <div class="link-box">
+                <a href="./php_Advanced_task/index.php">Check it!</a>
+            </div>
+        </div>
+
+    </div>
+
+    <script>
+        function toggleLinks(id) {
+            const linksDiv = document.getElementById(id);
+            if (linksDiv.style.display === "none" || linksDiv.style.display === "") {
+                linksDiv.style.display = "flex";
+            } else {
+                linksDiv.style.display = "none";
+            }
+        }
+    </script>
+</body>
+
+</html>
